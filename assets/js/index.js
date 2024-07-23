7b1bc029ad6417b165a5030080bb2d7778cb4842
Mihaly Lovrencsics <lomysa@gmail.com>
2024-07-23 22:11:43 +0000

    Solve array task nr.2

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,6 +40,13 @@ const people = [
 
   // Array.prototype.every() // is everyone 19 or older?
 
+  const isAdult3 = people.every(person => {
+    const currYear3 = (new Date()).getFullYear();
+    return currYear3 - person.year >=19;
+  });
+
+  console.log(isAdult3);  
+
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
